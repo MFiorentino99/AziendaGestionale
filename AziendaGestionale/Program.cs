32b70e77e227977a8f09ec7881445de3388d9838
MFiorentino99 <mfiorentino@studenti.apuliadigitalmaker.it>
2023-12-12 15:09:26 +0100

    correzione program.cs

--- AziendaGestionale/Program.cs
+++ AziendaGestionale/Program.cs
@@ -1,17 +1,13 @@
 using AziendaGestionale.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Data.OracleClient;
 
-//var _db = new Oracle.ManagedDataAccess.Client.
-//    OracleConnection("User Id=ITS_TEST;Password=ZexpDEV2224;Data Source=localhost");
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-//_db.Open();
-
+//builder.Services.AddDbContext<Context>(opt =>opt.UseInMemoryDatabase("DbAzienda"));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<Context>(opt =>opt.UseInMemoryDatabase("DbAzienda"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
